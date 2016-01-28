28fc78a3a3b8f9948395d9b9448b0635cf0dbb3e
Armifer <ios@armifer.com>
2016-01-28 08:21:52 +0100

    Added iOS Framework target.

---
+++ StyleKitSwiftExtractor/StyleKitGenerator/StyleKitGenerator.h
@@ -0,0 +1,19 @@
+//
+//  StyleKitGenerator.h
+//  StyleKitGenerator
+//
+//  Created by Armifer on 28/01/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyleKitGenerator.
+FOUNDATION_EXPORT double StyleKitGeneratorVersionNumber;
+
+//! Project version string for StyleKitGenerator.
+FOUNDATION_EXPORT const unsigned char StyleKitGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyleKitGenerator/PublicHeader.h>
+
+
---
+++ StyleKitSwiftExtractor/StyleKitViewGenerator/StyleKitViewGenerator.h
@@ -0,0 +1,19 @@
+//
+//  StyleKitViewGenerator.h
+//  StyleKitViewGenerator
+//
+//  Created by Armifer on 28/01/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StyleKitViewGenerator.
+FOUNDATION_EXPORT double StyleKitViewGeneratorVersionNumber;
+
+//! Project version string for StyleKitViewGenerator.
+FOUNDATION_EXPORT const unsigned char StyleKitViewGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyleKitViewGenerator/PublicHeader.h>
+
+
